05038321829f550293c09421c3f0251164714a1d
Richard Campion <richard.campion@zdslogic.com>
2023-12-17 21:26:14 -0500

    Initial commit

--- run-debug.sh
+++ run-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms8192m -Xmx16384m -Xss4096k -agentlib:jdwp=transport=dt_socket,address=9999,server=y,suspend=n -jar target/zdslogic-books-server-0.0.1-SNAPSHOT.jar
+java -Xms8192m -Xmx16384m -Xss4096k -agentlib:jdwp=transport=dt_socket,address=9999,server=y,suspend=n -jar target/books-server-0.0.1-SNAPSHOT.jar
